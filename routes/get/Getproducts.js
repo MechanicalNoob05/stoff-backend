5c9549cae7ada4f058bc8d9e147ff438ff41a66e
FIRST_NAME LAST_NAME <dsmanishsingh@gmail.com>
2023-06-16 16:15:21 +0530

    order addition

--- routes/get/Getproducts.js
+++ routes/get/Getproducts.js
@@ -4,8 +4,7 @@ const route = express.Router();
 route.post('/', async (req, res) => {
   try {
     const product = await Product.find().populate("store")
-    res.status(200).json({product})
-    //hello
+    res.status(200).json({ product })
   } catch (err) {
     res.json({ errors: err, message: err.message })
   }
